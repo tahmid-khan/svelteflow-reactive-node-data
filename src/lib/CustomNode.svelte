4a88cca3858775214deafd73abaa717d7d248cbe
Tahmid A. Khan <60913202+tahmid-khan@users.noreply.github.com>
2024-09-19 14:06:20 +0600

    Create the flow

---
+++ src/lib/CustomNode.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { type NodeProps } from '@xyflow/svelte';
+
+	type Data = { message: string };
+	type $$Props = NodeProps & { data: Data };
+
+	export let id: string;
+	export let data: Data;
+	const { message } = data;
+
+	const bgColor: string = id == 'node0' ? 'pink' : id == 'node1' ? 'lightgreen' : 'lightblue';
+</script>
+
+<div style:background-color={bgColor}>
+	{message}
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,80 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { writable, type Writable } from 'svelte/store';
+	import { SvelteFlow, Background } from '@xyflow/svelte';
+	import type { Edge, Node, NodeTypes } from '@xyflow/svelte';
+	import CustomNode from '$lib/CustomNode.svelte';
+
+	import '@xyflow/svelte/dist/style.css';
+
+	/**
+	 * Current time (integer) in the simulation
+	 */
+	let time: number = 0;
+
+	/**
+	 * Table of timestamped messages; structure: `<time>: { <node ID>: <message> }`
+	 */
+	const messages: Record<number, Record<string, string>> = {
+		0: {
+			'node0': 'node0: time = 0',
+			'node1': 'node1: time = 0',
+			'node2': 'node2: time = 0'
+		},
+		1: {
+			'node0': 'node0: time = 1',
+			'node1': 'node1: time = 1',
+			'node2': 'node2: time = 1'
+		},
+		2: {
+			'node0': 'node0: time = 2',
+			'node1': 'node1: time = 2',
+			'node2': 'node2: time = 2'
+		},
+		3: {
+			'node0': 'node0: time = 3',
+			'node1': 'node1: time = 3',
+			'node2': 'node2: time = 3'
+		},
+		4: {
+			'node0': 'node0: time = 4',
+			'node1': 'node1: time = 4',
+			'node2': 'node2: time = 4'
+		},
+		5: {
+			'node0': 'node0: time = 5',
+			'node1': 'node1: time = 5',
+			'node2': 'node2: time = 5'
+		}
+	};
+
+	const initialNodes: Node[] = ['node0', 'node1', 'node2'].map((id) => {
+		return {
+			id,
+			position: {
+				x: Math.random() * 150,
+				y: Math.random() * 150
+			},
+			data: {
+				message: messages[time][id]
+			},
+			type: 'node'
+		} satisfies Node;
+	});
+
+	const nodes: Writable<Node[]> = writable(initialNodes);
+	const edges: Writable<Edge[]> = writable([]);
+	const nodeTypes: NodeTypes = { node: CustomNode };
+</script>
+
+<main>
+	<SvelteFlow {nodes} {edges} {nodeTypes} fitView>
+		<Background />
+	</SvelteFlow>
+</main>
+
+<style>
+	main {
+		height: 100vh;
+		width: 150vh;
+	}
+</style>
